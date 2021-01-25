c9e558043758d9222fa5dc86264ab49a9e01b30d
Michel Belleville <michel@42network.org>
2021-01-25 20:04:08 +0100

    now with specific id stripper

--- lib/prometheus/client/forty_two.rb
+++ lib/prometheus/client/forty_two.rb
@@ -6,6 +6,22 @@ module Prometheus
     module FortyTwo
       module Middleware
         class Collector < Prometheus::Middleware::Collector
+          def initialize(app, options = {})
+            super
+
+            @specific_id_stripper = options[:specific_id_stripper] || ->(path) { path }
+          end
+
+          protected
+
+          def strip_ids_from_path(path)
+            stripped_path = super
+            begin
+              @specific_id_stripper.call(stripped_path)
+            rescue StandardError
+              stripped_path
+            end
+          end
         end
       end
     end
--- spec/prometheus/client/forty_two_spec.rb
+++ spec/prometheus/client/forty_two_spec.rb
@@ -10,10 +10,11 @@ describe Prometheus::Client::FortyTwo do
     let(:request_method) { 'POST' }
     let(:script_name) { 'SCRIPT_NAME' }
     let(:uuid) { SecureRandom.uuid }
-    let(:path_info) { "/blah/123/something/#{uuid}/or_other/123abc" }
+    let(:path_info) { "/blah/123/something/#{uuid}/users/123lautaro/b" }
     let(:metrics_prefix) { 'whatever' }
+    let(:duration) { 0.2 }
 
-    let(:cleaned_up_path) { '/blah/:id/something/:uuid/or_other/123abc' }
+    let(:cleaned_up_path) { '/blah/:id/something/:uuid/users/123lautaro/b' }
 
     let(:app) { double(:app) }
     let(:env) { double(:env) }
@@ -82,52 +83,152 @@ describe Prometheus::Client::FortyTwo do
         allow(response).to receive(:first)
           .and_return(code)
 
-        allow(app).to receive(:call)
-          .and_return(response)
+        allow(app).to receive(:call) do
+          sleep(duration)
+          response
+        end
 
         allow(requests_registry).to receive(:increment)
         allow(durations_registry).to receive(:observe)
       end
 
-      it 'collects a success like the original collector' do
-        collector =
-          Prometheus::Client::FortyTwo::Middleware::Collector.new(
-            app,
-            registry: registry,
-            metrics_prefix: metrics_prefix
-          )
+      context 'without specific id strippers' do
+        it 'collects a success like the original collector' do
+          collector =
+            Prometheus::Client::FortyTwo::Middleware::Collector.new(
+              app,
+              registry: registry,
+              metrics_prefix: metrics_prefix
+            )
 
-        expect(app).not_to have_received(:call)
+          expect(app).not_to have_received(:call)
 
-        result = collector.call(env)
+          result = collector.call(env)
 
-        expect(requests_registry).to have_received(:increment)
-          .once
-        expect(requests_registry).to have_received(:increment)
-          .with(
-            labels: {
-              code: code.to_s,
-              method: request_method.downcase,
-              path: cleaned_up_path
-            }
-          )
+          expect(requests_registry).to have_received(:increment)
+            .once
+          expect(requests_registry).to have_received(:increment)
+            .with(
+              labels: {
+                code: code.to_s,
+                method: request_method.downcase,
+                path: cleaned_up_path
+              }
+            )
 
-        expect(durations_registry).to have_received(:observe)
-          .once
-        expect(durations_registry).to have_received(:observe)
-          .with(
-            be_within(0.1).of(0),
-            labels: {
-              method: request_method.downcase,
-              path: cleaned_up_path
-            }
-          )
+          expect(durations_registry).to have_received(:observe)
+            .once
+          expect(durations_registry).to have_received(:observe)
+            .with(
+              be_within(0.001).of(duration),
+              labels: {
+                method: request_method.downcase,
+                path: cleaned_up_path
+              }
+            )
 
-        expect(app).to have_received(:call)
-          .once
-        expect(app).to have_received(:call)
-          .with(env)
-        expect(result).to eq response
+          expect(app).to have_received(:call)
+            .once
+          expect(app).to have_received(:call)
+            .with(env)
+          expect(result).to eq response
+        end
+      end
+
+      context 'with specific id strippers' do
+        context 'the stripper is not buggy' do
+          let(:cleaned_up_path) { '/blah/:id/something/:uuid/users/:id/b' }
+
+          it 'strips specific ids' do
+            collector =
+              Prometheus::Client::FortyTwo::Middleware::Collector.new(
+                app,
+                registry: registry,
+                metrics_prefix: metrics_prefix,
+                specific_id_stripper: lambda { |path|
+                  path.gsub(%r{/users/[^/]*}, '/users/:id')
+                }
+              )
+
+            expect(app).not_to have_received(:call)
+
+            result = collector.call(env)
+
+            expect(requests_registry).to have_received(:increment)
+              .once
+            expect(requests_registry).to have_received(:increment)
+              .with(
+                labels: {
+                  code: code.to_s,
+                  method: request_method.downcase,
+                  path: cleaned_up_path
+                }
+              )
+
+            expect(durations_registry).to have_received(:observe)
+              .once
+            expect(durations_registry).to have_received(:observe)
+              .with(
+                be_within(0.001).of(duration),
+                labels: {
+                  method: request_method.downcase,
+                  path: cleaned_up_path
+                }
+              )
+
+            expect(app).to have_received(:call)
+              .once
+            expect(app).to have_received(:call)
+              .with(env)
+            expect(result).to eq response
+          end
+        end
+
+        context 'the stripper is buggy' do
+          let(:exception) { ArgumentError.new('ooops') }
+
+          it 'does not fail, only uses the standard stripper' do
+            collector =
+              Prometheus::Client::FortyTwo::Middleware::Collector.new(
+                app,
+                registry: registry,
+                metrics_prefix: metrics_prefix,
+                specific_id_stripper: -> { raise exception }
+              )
+
+            expect(app).not_to have_received(:call)
+
+            result = collector.call(env)
+
+            expect(requests_registry).to have_received(:increment)
+              .once
+            expect(requests_registry).to have_received(:increment)
+              .with(
+                labels: {
+                  code: code.to_s,
+                  method: request_method.downcase,
+                  path: cleaned_up_path
+                }
+              )
+
+            expect(durations_registry).to have_received(:observe)
+              .once
+            expect(durations_registry).to have_received(:observe)
+              .with(
+                be_within(0.001).of(duration),
+                labels: {
+                  method: request_method.downcase,
+                  path: cleaned_up_path
+                }
+              )
+
+            expect(app).to have_received(:call)
+              .once
+            expect(app).to have_received(:call)
+              .with(env)
+            expect(result).to eq response
+          end
+        end
       end
     end
 
@@ -141,28 +242,59 @@ describe Prometheus::Client::FortyTwo do
         allow(exceptions_registry).to receive(:increment)
       end
 
-      it 'collects a failure like the original collector' do
-        collector =
-          Prometheus::Client::FortyTwo::Middleware::Collector.new(
-            app,
-            registry: registry,
-            metrics_prefix: metrics_prefix
-          )
+      context 'without specific id strippers' do
+        it 'collects a failure like the original collector' do
+          collector =
+            Prometheus::Client::FortyTwo::Middleware::Collector.new(
+              app,
+              registry: registry,
+              metrics_prefix: metrics_prefix
+            )
 
-        expect(app).not_to have_received(:call)
+          expect(app).not_to have_received(:call)
 
-        expect { collector.call(env) }
-          .to raise_error(exception)
+          expect { collector.call(env) }
+            .to raise_error(exception)
 
-        expect(exceptions_registry).to have_received(:increment)
-          .once
-        expect(exceptions_registry).to have_received(:increment)
-          .with(labels: { exception: 'ArgumentError' })
+          expect(exceptions_registry).to have_received(:increment)
+            .once
+          expect(exceptions_registry).to have_received(:increment)
+            .with(labels: { exception: 'ArgumentError' })
 
-        expect(app).to have_received(:call)
-          .once
-        expect(app).to have_received(:call)
-          .with(env)
+          expect(app).to have_received(:call)
+            .once
+          expect(app).to have_received(:call)
+            .with(env)
+        end
+      end
+
+      context 'with specific id strippers' do
+        it 'collects a failure like the original collector' do
+          collector =
+            Prometheus::Client::FortyTwo::Middleware::Collector.new(
+              app,
+              registry: registry,
+              metrics_prefix: metrics_prefix,
+              specific_id_stripper: lambda { |path|
+                path.gsub(%r{/users/[^/]*}, '/users/:id')
+              }
+            )
+
+          expect(app).not_to have_received(:call)
+
+          expect { collector.call(env) }
+            .to raise_error(exception)
+
+          expect(exceptions_registry).to have_received(:increment)
+            .once
+          expect(exceptions_registry).to have_received(:increment)
+            .with(labels: { exception: 'ArgumentError' })
+
+          expect(app).to have_received(:call)
+            .once
+          expect(app).to have_received(:call)
+            .with(env)
+        end
       end
     end
   end
